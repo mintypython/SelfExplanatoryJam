118f64d2c508ae9cfaf392e99f24e48d66294105
Mimpy <nkiley96@gmail.com>
2026-05-10 00:03:34 -0400

    Add trophy button, rescale ui windows to match camera

---
+++ Assets/Scripts/UI/TrophyButton.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class TrophyButton : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public void Click() {
+        Debug.Log("Click!!");
+    }
+}
